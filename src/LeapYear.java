2b36dc5a86c8125c01e2560eabc72750601bd8be
AdmiN <AdmiN@ITWS062>
2024-08-20 10:59:04 +0530

    commit the data

---
+++ src/LeapYear.java
@@ -0,0 +1,20 @@
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		int a =2003;
+		if(a%4==0 && a%100!=0 || a%400==0)
+		{
+			System.out.println("leap year");
+		}
+		else
+		{
+			System.out.println("not a leap year");
+		}
+		
+		
+		
+
+	}
+
+}
